2b2413fd40486a302cd7fc7dee00cf77f144808d
marianoariassoy <hola@marianoarias.soy>
2026-04-15 18:41:10 -0300

    first commit

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -3,7 +3,7 @@ const Banner = () => {
     <div
       className="aspect-7/5 lg:aspect-16/5 bg-cover bg-center bg-fixed"
       style={{
-        backgroundImage: `url("https://backend.ligadecapitanes.com.ar/rise/images-static/home.jpg?v=15")`,
+        backgroundImage: `url("https://backend.riiseyasociados.com.ar/images-static/home.jpg?v=10")`,
       }}
     />
   );
